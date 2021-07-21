63c52463af0402b1f91de3c3526a6fb5b5cd91f8
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-07-21 17:49:57 +0400

    set up chakra

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Container>
+      <Heading>Courier</Heading>
+      <Text>
+        Welcome to the React In-App Toast App. Select the options for your toast
+        and try it below.
+      </Text>
+    </Container>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>init</div>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
