da97a6312ba9ed1cfa880659e952059d4a11e017
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-07-22 20:28:12 +0400

    set up courier provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ToastProvider } from "@trycourier/react-toast";
+import { Toast } from "@trycourier/react-toast";
+import { CourierProvider } from "@trycourier/react-provider";
 
 import App from "./App";
+
+const COURIER_CLIENT_KEY = "";
+const COURIER_USER_ID = "";
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <ToastProvider>
+      <CourierProvider clientKey={COURIER_CLIENT_KEY} userId={COURIER_USER_ID}>
+        <Toast />
+
         <App />
-      </ToastProvider>
+      </CourierProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
